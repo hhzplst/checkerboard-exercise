09665dd6e966365bf36f4b0be4fe6c07a29cc2f6
Ivy Chang <ivycwq@icloud.com>
2015-08-17 11:25:23 -0700

    random colors

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 // Your JS goes here
+
+for(var i=0; i<100; i++){
+	var newDiv = document.createElement("div"); 
+	newDiv.style.float = "left";
+	newDiv.style.paddingBotom = "11.1%";
+	newDiv.style.width = "11.1%";
+	newDiv.style.height = "150px";
+
+	if(i%2 === 0){
+		newDiv.style.backgroundColor = getRandomColor();
+		document.body.appendChild(newDiv);
+	}else{
+		newDiv.style.backgroundColor = getRandomColor();
+		document.body.appendChild(newDiv);
+	}
+}
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
